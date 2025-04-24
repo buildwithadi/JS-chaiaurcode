b35f466a837929e3c6a99fba650fa9a23a645228
buildwithadi <adityarawat.online@gmail.com>
2025-04-24 09:40:53 +0530

    1 basics

---
+++ 1basics/2variables.js
@@ -0,0 +1,12 @@
+const accountId = 12345
+var accountName = "Aditya"
+let accountPassword = "12345"
+accountCity = "Dehradun"
+
+console.table([accountId, accountName,accountPassword,accountCity])
+
+accountName = "Aman"
+accountPassword = "54321"
+accountCity = "Haridwar"
+
+console.table([accountId, accountName,accountPassword,accountCity])
---
+++ 1basics/3datatypes.js
@@ -0,0 +1,19 @@
+let name = "hitesh"
+let age = 18
+let inLoggedIn = false
+
+// number => 2 to power 53
+// bigint
+// string = ""
+// boolean => true/false
+// null => standalone value
+// undefined => 
+// symbol => unique
+
+// object
+
+console.log(typeof "This is a String");
+console.log(typeof age);
+console.log(typeof null); // This is an object 
+console.log(typeof undefined)
+
